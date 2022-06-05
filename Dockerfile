69564ae77697e3ec5def680bf84cb9e6fe908140
soomro-abd <24100180@lums.edu.pk>
2022-06-05 23:52:02 +0500

    added the dockerfile and a controller script file

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM python:3.8
+
+COPY yolov5 yolov5
+COPY master.py .
+
+# RUN pip3 install -r ./yolov5/requirements.txt
+RUN pip3 install matplotlib>=3.2.2
+RUN pip3 install numpy>=1.18.5
+RUN pip3 install opencv-python>=4.1.1
+RUN pip3 install Pillow>=7.1.2
+RUN pip3 install PyYAML>=5.3.1
+RUN pip3 install requests>=2.23.0
+RUN pip3 install scipy>=1.4.1
+RUN pip3 install torch>=1.7.0
+RUN pip3 install torchvision>=0.8.1
+RUN pip3 install tqdm>=4.41.0
+RUN pip install protobuf==3.20.1
+RUN pip3 install tensorboard>=2.4.1
+RUN pip3 install pandas>=1.1.4
+RUN pip3 install seaborn>=0.11.0
+RUN pip3 install ipython
+RUN pip3 install psutil
+RUN pip3 install thop
+
+RUN apt-get update
+RUN apt-get install -y libgl1
---
+++ controller.py
@@ -0,0 +1,79 @@
+"""
+This script calls the YOLOv5 model and returns the results in the form of an image. 
+"""
+
+import torch
+from matplotlib import pyplot as plt
+import numpy as np
+import cv2
+
+
+def handleImage(model, file_name):
+
+    # running the model on the image
+    results = model(file_name)
+
+    # printing the results returned by the model
+    results.print()
+
+    results.show()
+
+    # displaying the results in image format
+    # while True:
+    #     RGB_img = cv2.cvtColor(np.squeeze(results.render()), cv2.COLOR_BGR2RGB)
+    #     cv2.imshow("YOLO", RGB_img)
+
+    #     if cv2.waitKey(10) & 0xFF == ord('q'):
+    #         break
+
+    # cv2.destroyAllWindows()
+    
+
+    return
+
+def handleVideo(model, file_name):
+    cap = cv2.VideoCapture(file_name)
+    while cap.isOpened():
+        ret, frame = cap.read()
+        
+        #Make detections
+        results = model(frame)
+
+        cv2.imshow('YOLO', np.squeeze(results.render()))
+        
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+            
+    cap.release()
+    cv2.destroyAllWindows() 
+
+    return
+
+
+
+def main():
+
+    # loading the yolov5s model
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+    
+    # storing the name of the file on which detection would run
+    file_name = input("Filename to run detection: ")
+
+    # extracting the last three characters to find out wether its an image or a video
+    last_three_chars = file_name[-3:]
+
+    if last_three_chars == "mp4":
+        
+        # process for video
+        handleVideo(model, file_name)
+    elif last_three_chars == "jpg":
+
+        # process for image
+        handleImage(model, file_name)
+    else:
+        print("ERROR! Filename is not correct. Only .jpg and .mp4 are supported.")
+        return
+
+
+if __name__ == "__main__":
+    main()
